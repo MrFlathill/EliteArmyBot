722d5ca6327ca4139dd1ea6b184f6fa1948299f0
MrFlathill <FlathillJR@gmail.com>
2021-04-26 23:37:27 +0200

    added temprature and clock mesuremant commands

---
+++ commands/temp.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'temp',
+    aliases: ['clock'],
+    description: "Gives Server Status (raspberry pi)",
+    async execute(message, args, cmd, Discord){
+        const execSync = require('child_process').execSync;
+        // import { execSync } from 'child_process';  // replace ^ if using ES modules
+        
+        if (cmd === 'temp') {
+            output = execSync('vcgencmd measure_temp', { encoding: 'utf-8' });  // the default is 'buffer'
+            return message.channel.send(output);
+
+        }else if (cmd === 'clock') {
+            output = execSync('vcgencmd measure_clock arm', { encoding: 'utf-8' });
+            return message.channel.send(output);
+
+        } else {
+            return message.reply('Wrong Argument!');
+        }
+    }
+}
