1702c629ee40387679247bfdc0269ef8b94c4ed5
Nis5l <kirchler123@gmail.com>
2021-09-30 08:10:06 +0200

    fix: typos and small code improvement

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports = {
         if(!args[0]) return message.reply("please enter the amount of messages that you want to clear!");
         if(isNaN(args[0])) return message.reply("please enter a real number!");
 
-        if(args[0] > 100) return message.reply("You canno't delete more then 100 masseges!");
+        if(args[0] > 100) return message.reply("You cannot delete more then 100 messages!");
         if(args[0] < 1) return message.reply("You must delete at least one message!");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
